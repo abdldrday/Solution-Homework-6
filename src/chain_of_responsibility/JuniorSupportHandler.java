2af673d16cd1a383024d57b019b4b15899f96c90
abdldrday <abdildirday14@gmail.com>
2025-04-20 15:44:10 +0500

    juniorsupport class and new test class

--- src/chain_of_responsibility/JuniorSupportHandler.java
+++ src/chain_of_responsibility/JuniorSupportHandler.java
@@ -2,6 +2,12 @@ package chain_of_responsibility;
 
 public class JuniorSupportHandler extends AbstractSupportHanler{
     public void handle(String issue){
-        if(issue.equals("refund_request"))
+        if(issue.equals("refund_request") || issue.equals("billing_issue")){
+            System.out.println("[JunoirSupport] Handled " + issue);
+        } else if (nextHandler != null){
+            nextHandler.handle(issue);
+        } else {
+            System.out.println("[JuniourSupport] can't handle" + issue);
+        }
     }
 }
