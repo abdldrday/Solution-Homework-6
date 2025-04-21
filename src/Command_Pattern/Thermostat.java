3a89ff796508f039e421891a82dc15cfe1cfa4cd
abdldrday <abdildirday14@gmail.com>
2025-04-21 21:58:16 +0500

    light and thermostat classes added

--- src/Command_Pattern/Thermostat.java
+++ src/Command_Pattern/Thermostat.java
@@ -1,4 +1,17 @@
 package Command_Pattern;
 
 public class Thermostat {
+    private int previousTemp;
+    private int currentTemp;
+
+    public void setTemperature(int temp) {
+        previousTemp = currentTemp;
+        currentTemp = temp;
+        System.out.println("[Thermostat] Setting temperature to " + temp + "°C");
+    }
+
+    public void revertTemperature() {
+        currentTemp = previousTemp;
+        System.out.println("[Thermostat] Reverting to previous temperature");
+    }
 }
